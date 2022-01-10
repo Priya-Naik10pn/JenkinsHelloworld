b0dbbafe96a161292f40370ee12ed316dcc2337a
Priya-Naik10pn <74342924+Priya-Naik10pn@users.noreply.github.com>
2022-01-10 12:41:20 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,6 +3,6 @@ package project;
 public class Helloworld {
 	public static void main(String[] args)
 	{
-		System.out.print("Hello World ...!!");
+		System.out.print("Hello World and welcome to jenkins ...!!");
 	}
 }
